c259fd8e657c4c04b398488903db0d8730ec2bca
Junaid Khan <157748393+junaidify@users.noreply.github.com>
2026-05-11 16:03:38 +0530

    feat/auth_schema and api designed

---
+++ src/main/java/learn_vault/controller/UserController.java
@@ -0,0 +1,29 @@
+package learn_vault.controller;
+
+import jakarta.validation.Valid;
+import learn_vault.dto.UserRegisterDto;
+import learn_vault.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+//    private final UserService userService;
+//
+//    public UserController(UserService userService){
+//        this.userService = userService;
+//    }
+
+    // We can initilize two ways, constructor injection is mentioned above and below one is called field injection.
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public String userRegister(@Valid @RequestBody UserRegisterDto dto){
+        return userService.userRegister(dto);
+    }
+
+
+}
---
+++ src/test/java/learn_vault/LearnVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package learn_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
