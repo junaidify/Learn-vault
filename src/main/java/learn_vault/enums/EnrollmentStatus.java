7faeb6f8a1bee4013a7f4654d15da72bc93c257a
Junaid Khan <157748393+junaidify@users.noreply.github.com>
2026-06-12 22:34:42 +0530

    Delete src directory

--- src/main/java/learn_vault/enums/EnrollmentStatus.java
+++
@@ -1,5 +0,0 @@
-package learn_vault.enums;
-
-public enum EnrollmentStatus {
-    ACTIVE, EXPIRED, CANCELLED
-}
--- src/main/java/learn_vault/enums/Role.java
+++
@@ -1,5 +0,0 @@
-package learn_vault.enums;
-
-public enum Role {
-    STUDENT, AUTHOR, ADMIN;
-}
